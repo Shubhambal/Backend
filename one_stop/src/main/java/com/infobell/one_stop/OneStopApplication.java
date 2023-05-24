dc8eb43dbd2a514581ea3646daf980fe352907b7
sumukh-gn <mesumukh@gmail.com>
2023-05-24 10:04:05 +0530

    Backend model class

---
+++ one_stop/src/main/java/com/infobell/one_stop/OneStopApplication.java
@@ -0,0 +1,13 @@
+package com.infobell.one_stop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneStopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneStopApplication.class, args);
+	}
+
+}
